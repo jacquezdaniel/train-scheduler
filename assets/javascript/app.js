30bd9f58dd8e9f9bd0038f9ca8b524d6f26acb51
jacquezdaniel <jacquez.j.daniel@gmail.com>
2018-11-18 17:55:47 -0800

    moment

---
+++ assets/javascript/app.js
@@ -0,0 +1,78 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBDZSJUZynVqoD1rEOJLRaJS8wfNVgf8a4",
+    authDomain: "djproject1-66de0.firebaseapp.com",
+    databaseURL: "https://djproject1-66de0.firebaseio.com",
+    projectId: "djproject1-66de0",
+    storageBucket: "djproject1-66de0.appspot.com",
+    messagingSenderId: "831629948512"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+var trainName = "";
+var destination = "";
+var firstTrainTime = "";
+var frequency = 0;
+
+
+// FUNCTIONS + EVENTS
+$("#addTrain").on("click", function(event) {
+  event.preventDefault();
+
+  trainName = $('#nameInput').val().trim();
+  destination = $('#destinationInput').val().trim();
+  firstTrainTime = $('#firstTrainInput').val().trim();
+  frequency = $('#frequencyInput').val().trim();
+
+  console.log(trainName);
+  console.log(destination);
+  console.log(firstTrainTime);
+  console.log(frequency);
+
+  database.ref().push({
+    trainName: trainName,
+    destination: destination,
+    firstTrainTime: firstTrainTime,
+    frequency: frequency
+  });
+
+    return false;
+});
+
+
+// MAIN PROCESS + INITIAL CODE
+database.ref().on("child_added", function(snapshot) {
+  console.log(snapshot.val());
+
+  // update the variable with data from the database
+  trainName = snapshot.val().trainName;
+  destination = snapshot.val().destination;
+  firstTrainTime = snapshot.val().firstTrainTime;
+  frequency = snapshot.val().frequency;
+
+
+  var firstTrainMoment = moment(firstTrainTime, 'HH:mm');
+  var nowMoment = moment();
+
+  var minutesSinceFirstArrival = nowMoment.diff(firstTrainMoment, 'minutes');
+  var minutesSinceLastArrival = minutesSinceFirstArrival % frequency;
+  var minutesAway = frequency - minutesSinceLastArrival;
+
+  var nextArrival = nowMoment.add(minutesAway, 'minutes');
+  var formatNextArrival = nextArrival.format("HH:mm");
+
+
+  // create new row
+  var newRow = $("<tr>").append(
+  $("<td>").text(trainName),
+  $("<td>").text(destination),
+  $("<td>").text(frequency),
+  $("<td>").text(formatNextArrival),
+  $("<td>").text(minutesAway)
+  );
+
+  $("#trains-table > tbody").append(newRow);
+
+});
